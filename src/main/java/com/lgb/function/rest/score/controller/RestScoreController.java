636cd3055b74cd6ab34391ff17e3e040390ed550
wenge <1980849329@qq.com>
2016-06-06 21:32:55 +0800

    score finished third

--- src/main/java/com/lgb/function/rest/score/controller/RestScoreController.java
+++ src/main/java/com/lgb/function/rest/score/controller/RestScoreController.java
@@ -5,6 +5,7 @@ import com.lgb.function.rest.score.service.RestScoreServiceI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,6 +17,7 @@ public class RestScoreController {
     @Autowired
     private RestScoreServiceI restScoreService;
 
+    @RequestMapping(value = "/{userCardNum}", method = RequestMethod.GET)
     public List<RestStudentScoreInfo> getStuAllScore(@PathVariable("userCardNum") String userCardNum) {
         return restScoreService.selectStuScore(userCardNum);
     }
